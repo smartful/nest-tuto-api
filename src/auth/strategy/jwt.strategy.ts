ecab0fd8fbf52700ecf237c5bb0b1d4d4984adf9
smartful <remi_rodrigues@hotmail.com>
2022-09-02 10:55:10 +0200

    auth strategy feature

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import config from '../../config/keys';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.JWT_SECRET,
     });
+  }
+
+  validate(payload: any) {
+    return payload;
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   exports: [MongooseModule],
+  controllers: [UserController],
 })
 export class UserModule {}
